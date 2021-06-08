dbcc37fc173c51ce4ccb8174774b46cb052dc9ed
Brian Childress <brian@brianchildress.co>
2021-06-07 22:08:47 -0400

    Updating Stryker config

--- stryker.conf.js
+++ stryker.conf.js
@@ -2,12 +2,12 @@ module.exports = (config) => {
   config.set({
     coverageAnalysis: "perTest",
     maxConvurrentTestRunners: 4,
-    mutate: ["/api/range/js", "!/api/styker.*", "/api/**/*.spec.js"],
+    mutate: ["/api/range.js", "!/api/styker.*", "!/api/**/*.spec.js"],
     mutator: "javascript",
     packageManager: "npm",
     reporters: ["clear-text", "progress", "html"],
     testFramework: "jasmine",
-    commandRunner: { command: 'npm run test:unit' },
-    symlinkNodeModules: false
-  })
-}
+    commandRunner: { command: "npm run test:unit" },
+    symlinkNodeModules: false,
+  });
+};
